1702964d3029f79c76786374a978d50110ed5cae
Ashwani Patel <ashwanipatel_2007@yahoo.co.in>
2025-03-06 23:25:40 +0530

    Initial commit

---
+++ src/main/java/com/practice/practoapp/PractoappApplication.java
@@ -0,0 +1,14 @@
+package com.practice.practoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PractoappApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/practoapp/PractoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.practoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
